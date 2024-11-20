07fc916b3c05455899f729fa3306d1357db255df
Isabella062 <100538085+Isabella062@users.noreply.github.com>
2024-11-20 12:47:00 -0300

    feat: I changed the name of the pages and created the "details" file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="register"
         options={{
-          title: 'Explore',
+          title: 'register',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
