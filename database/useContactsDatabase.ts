8592d6f3c4c45a4e9b11ee3d41f81d622310f5e2
Gabriel Lira <glcalil10@gmail.com>
2024-12-04 22:47:40 -0300

    add ordenação na função findAll e documentação do código

--- database/useContactsDatabase.ts
+++ database/useContactsDatabase.ts
@@ -10,6 +10,11 @@ export type Contact = {
 export function useContactsDatabase(){
     const dbAdapter = useSQLiteContext();
     
+    /** Função para criar um novo contato na lista de contatos
+     *  @param data 
+     *   - Paremetro com um objeto com as mesmas propriedades de Contact com execeção id  
+     *  @returns id do novo contato inserido na lista de contatos 
+     */
     async function create(data: Omit<Contact, "id">){
         validateContact(data);
 
@@ -34,11 +39,14 @@ export function useContactsDatabase(){
 
     }
 
+    /** Função para buscar todos os contatos salvos na lista
+     *  
+     *  @returns Lista de contatos ordenados em ordem alfabetica, com a penas a informação de nome e id de cada contato
+     */
     async function findAll(): Promise<Contact | Contact[] | null> {
         try {
-            const result = await dbAdapter.getAllAsync<Contact>(`SELECT id, name FROM contact`);
-    
-            // Retornar todos os resultados como um array
+            const result = await dbAdapter.getAllAsync<Contact>(`SELECT id, name FROM contact ORDEM BY name ASC`);
+
             return result;
         } catch (error) {
             console.error('Erro ao executar consulta:', error);
@@ -46,6 +54,10 @@ export function useContactsDatabase(){
         }
     }
 
+    /** Função para buscar todos os dados de um contato
+     *  @param id  - id do contato
+     *  @returns Retorna as informações completas do contato buscado
+     */
     async function findById(id: number): Promise<Contact | null>{
         try {
             const result = await dbAdapter.getFirstAsync<Contact>(`SELECT id, name, phone, email FROM contact WHERE id = ?`, id);
@@ -58,6 +70,12 @@ export function useContactsDatabase(){
         }
     }
 
+    /** Função para atualizar os dados de um contato
+     *  @param data - Dados (incluindo id) do contato que será atualizado no banco de dados
+     *  @returns Retorna se a atualização dos dados foi realizada com sucesso ou um exceção em caso de falha;
+     * 
+     *  `Obs: Em sistemas mais sensiveis, uma tabela de historico é recomendada para manter controle das informações de alterações`
+     */
     async function updateOne(data: Contact ): Promise<Boolean | Error>{
         validateContact(data);
         const stmt = await dbAdapter.prepareAsync("UPDATE contact SET name = $name, phone = $phone, email = $email WHERE id = $id");
@@ -79,6 +97,11 @@ export function useContactsDatabase(){
        
     }
 
+
+     /** Função para remover um contato da lista de contatos
+     *  @param id -  id do contato que deve ser removido
+     *  @returns Retorna se a remoção foi realizada com sucesso ou um exceção em caso de falha;
+     */
     async function deleteOne(id: number): Promise<Boolean | Error>{
         const stmt = await dbAdapter.prepareAsync("DELETE FROM contact WHERE id = $id");
         try {
@@ -95,6 +118,10 @@ export function useContactsDatabase(){
         }
     }
 
+    /** Função para buscar todos os dados de um contato
+     *  @param data - Dados do contato que deve ser validado
+     *  @returns Retorna as sucesso caso os dados sejam validos ou exceção caso exista alguma incoerencia nos dados
+     */
     function validateContact(contact: Contact | Omit<Contact, "id">): boolean {
         const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!regexEmail.test(contact.email)) throw new Error("Email informado invalido");
